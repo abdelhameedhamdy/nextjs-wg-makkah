25cc00f76e4beca777ba733a0edf625557d005e9
abdelhameedalsayed <abdulhameed.azab@gmail.com>
2023-03-01 21:41:19 +0200

    fixing types issue _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
 import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <main className="flex justify-center min-h-screen dark:bg-slate-800">
       <Component {...pageProps} />
